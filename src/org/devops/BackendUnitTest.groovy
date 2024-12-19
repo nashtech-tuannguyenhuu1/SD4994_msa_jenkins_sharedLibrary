8b15fd524a0d5a472cfd98657cc44988ce064c1d
tuannguyenh1 <tuannguyenh1@nashtechglobal.com>
2024-12-19 08:55:12 +0700

    yy

--- src/org/devops/BackendUnitTest.groovy
+++ src/org/devops/BackendUnitTest.groovy
@@ -6,8 +6,15 @@ def runUnitTests() {
         script {
             if (fileExists('package.json')) {
 
-                sh 'npm install'
-                sh 'npm test'
+                if (isUnix()) {
+                    // Dành cho Linux/Mac
+                    sh 'npm install'
+                    sh 'npm test'
+                } else {
+                    // Dành cho Windows
+                    bat 'npm install'
+                    bat 'npm test'
+                }
 
             } else {
                 error "No package.json file found. Unable to run tests."
