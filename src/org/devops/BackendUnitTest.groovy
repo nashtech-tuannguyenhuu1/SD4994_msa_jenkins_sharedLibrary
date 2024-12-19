0644c9988bd0f60e1b37cc4251a8a76613a0a047
tuannguyenh1 <tuannguyenh1@nashtechglobal.com>
2024-12-19 09:04:07 +0700

    gg

--- src/org/devops/BackendUnitTest.groovy
+++ src/org/devops/BackendUnitTest.groovy
@@ -9,11 +9,11 @@ def runUnitTests() {
                 if (isUnix()) {
                     // Dành cho Linux/Mac
                     sh 'npm install'
-                    sh 'npm test'
+                  
                 } else {
                     // Dành cho Windows
                     bat 'npm install'
-                    bat 'npm test'
+          
                 }
 
             } else {
